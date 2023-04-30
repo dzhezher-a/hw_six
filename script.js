d3c8879f9f2eec9fb918a53b3f7235cc7d98190f
dzhezher-a <121169002+dzhezher-a@users.noreply.github.com>
2023-04-30 02:33:53 +0200

    second commit

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
-function promptAge(promptMessage) {
-  let input = prompt(promptMessage);
+function userPortraitApp() {
+  function promptAge() {
+    let input = prompt("Введіть ваш рік народження: ");
 
-  while (
-    isNaN(parseInt(input)) ||
-    parseInt(input) < 1900 ||
-    parseInt(input) > 2020 ||
-    input.trim() === ""
-  ) {
-    input = prompt("Некоректне значення. Введіть вік ще раз.");
+    while (
+      isNaN(parseInt(input)) ||
+      parseInt(input) < 1900 ||
+      parseInt(input) > 2020 ||
+      input.trim() === ""
+    ) {
+      if (input === null) {
+        alert("Шкода, що Ви не захотіли ввісти свій рік.");
+        return "?";
+      }
+      input = prompt("Некоректне значення. Введіть рік народження ще раз: ");
+    }
+
+    return 2023 - Number(input);
   }
 
-  return 2023 - Number(input);
-}
+  function promptCity() {
+    let input = prompt("Введіть ваше місто: ");
 
-function promptCity(promptMessage) {
-  let input = prompt(promptMessage);
+    while (input !== / ^[А-ЯҐЄІЇ]+$/ || input.trim() === "") {
+      if (input === null) {
+        alert("Шкода, що Ви не захотіли ввісти своє місто.");
+        return "?";
+      } else if (input === ("Київ" || "Вашингтон" || "Лондон")) {
+        return `Ви живете у столиці ${input}`;
+      }
+      input = prompt("Некоректне значення. Введіть місто ще раз: ");
+    }
 
-  while (input === /[^A-z]/ || input.trim() === "") {
-    input = prompt("Некоректне значення. Введіть місто ще раз.");
+    return `Ви живете у місті ${input}`;
   }
 
-  return input;
-}
+  function promptSport() {
+    let input = prompt("Введіть ваш улюблений від спорту: ");
 
-function promptSport(promptMessage) {
-  let input = prompt(promptMessage);
+    while (input !== /\p{IsCyrillic}/ || input.trim() === "") {
+      if (input === null) {
+        alert("Шкода, що Ви не захотіли ввісти свій улюблений від спорту.");
+        return "Завдання на сьогодні: вийти на вулицю та почати бійку з випадковим перехожим";
+      } else if (input === "футбол") {
+        return `Круто! бажаєте стати К'єліні?`;
+      } else if (input === "баскетбол") {
+        return `Круто! бажаєте стати Джорданом?`;
+      } else if (input === "бокс") {
+        return `Круто! бажаєете стати Пак'яо?`;
+      }
+      input = prompt("Некоректне значення. Введіть від спорту ще раз: ");
+    }
 
-  while (input === /[^A-z]/ || input.trim() === "") {
-    input = prompt("Некоректне значення. Введіть місто ще раз.");
+    return `Бажаю успіхів у ${input}і`;
   }
 
-  return input;
+  let userAge = promptAge();
+  let userCity = promptCity();
+  let userSport = promptSport();
+
+  return alert(`Вам ${userAge} років та ${userCity}. ${userSport}.`);
 }
 
-function userCity() {
-  if (promptCity === ("Kyiv" || "Washington" || "London")) {
-    return `Ти живеш у столиці ${promptCity}`;
-  }
-  return `ти живеш у місті ${promptCity}`;
-}
+let userPortrait = userPortraitApp();
